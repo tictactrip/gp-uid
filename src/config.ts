c0246ef3f469f5ca77320bb56bbff5aa17a986a5
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2019-07-16 15:24:01 +0200

    feat(config.ts) file created

---
+++ src/config.ts
@@ -0,0 +1,36 @@
+interface IConfiguration {
+  cluster: {
+    prefix: string;
+    coord_precision: number;
+    hash_precision: number;
+    name_length: number;
+  };
+  group: {
+    prefix: string;
+    coord_precision: number;
+    hash_precision: number;
+    name_length: number;
+  },
+  require_field: string[],
+}
+
+const configuration: IConfiguration = {
+  cluster: {
+    prefix: 'c|',
+    coord_precision: 2,
+    hash_precision: 5,
+    name_length: 8,
+  },
+  group: {
+    prefix: 'g|',
+    coord_precision: 4,
+    hash_precision: 6,
+    name_length: 8,
+  },
+  require_field: ['name', 'type', 'latitude', 'longitude', 'country_code'],
+};
+
+export {
+  IConfiguration,
+  configuration,
+}
